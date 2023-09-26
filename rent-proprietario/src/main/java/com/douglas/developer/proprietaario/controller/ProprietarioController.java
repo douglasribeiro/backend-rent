954b1ba39e39fa5e1e50f1e478f343a193c87064
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2023-09-26 14:48:02 -0300

    atualização protocolo

--- rent-proprietario/src/main/java/com/douglas/developer/proprietaario/controller/ProprietarioController.java
+++ rent-proprietario/src/main/java/com/douglas/developer/proprietaario/controller/ProprietarioController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +30,7 @@ public class ProprietarioController implements ControllerBasic<Proprietario> {
 
     @Override
     @RolesAllowed("backend-admin")
+    @PreAuthorize("hasRole('ADMINS')")
     public ResponseEntity<String> status() {
         log.info("Serviço de proprietario esta ok!");
         return new ResponseEntity<>("Serviço de proprietario esta ok!",HttpStatus.OK);
